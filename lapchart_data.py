234f5334c283d27f1ab60fe0f0cba27ca8f281e5
Jeffrey Hutzelman <jhutz@cmu.edu>
2017-08-27 19:18:32 -0400

    Normalize comparisons to None

--- lapchart_data.py
+++ lapchart_data.py
@@ -7,18 +7,18 @@ class chartcar:
         self._class = None
 
     def car_no(self, val=None):
-        if val != None:
+        if val is not None:
             self._car_no = val
             self.parent.refresh_gui_for_car(self)
         return self._car_no
 
     def laps(self, val=None):
-        if val != None:
+        if val is not None:
             self._laps = val
         return self._laps
 
     def class_(self, val=None):
-        if val != None:
+        if val is not None:
             self._class = val
             self.parent.refresh_gui_for_car(self)
         return self._class
@@ -45,13 +45,13 @@ class chartdatacell:
         if self.gui: self.gui.update()
 
     def car(self, val=None):
-        if val != None:
+        if val is not None:
             self._car = val
             if self.gui: self.gui.update()
         return self._car
 
     def lead(self, val=None):
-        if val != None:
+        if val is not None:
             self._lead = val
             if self.gui: self.gui.update()
         return self._lead
@@ -75,7 +75,7 @@ class chartdata:
         return len(self.cells)
 
     def max_pos(self, lap=None):
-        if lap == None: return self.maxpos
+        if lap is None: return self.maxpos
         if lap > len(self.cells): return None
         return len(self.cells[lap-1])
 
@@ -88,7 +88,7 @@ class chartdata:
         car = self.car(car_id, car_no=car_id, create=True)
 
         # Determine lap and make sure we have enough columns
-        if lap == None:
+        if lap is None:
             lap = car.laps() + 1
         if lap > len(self.cells):
             self.cells.extend([[] for i in range(lap - len(self.cells))])
@@ -96,18 +96,18 @@ class chartdata:
             car.laps(lap)
 
         # Determine lead lap
-        if lead == None:
+        if lead is None:
             lead = len(self.cells)
             if lead < lap: lead = lap
 
         # Determine position and make sure we have enough cells
-        if pos == None:
+        if pos is None:
             pos = self.max_pos(lap) + 1
         if pos > self.maxpos:
             self.maxpos = pos
         if pos > self.max_pos(lap):
             self.cells[lap-1].extend([None] * (pos - self.max_pos(lap)))
-        if self.cells[lap-1][pos-1] == None:
+        if not self.cells[lap-1][pos-1]:
             self.cells[lap-1][pos-1] = chartdatacell(self, lap, pos, self.gui)
 
         # Update the cell
